c24e4ba5a499fe6990eec5366619f4be0f46d987
VIVEK JANGID <rajtrade.inc@gmail.com>
2022-10-06 14:51:43 +0530

    base uri

--- backend/src/config.js
+++ backend/src/config.js
@@ -81,7 +81,7 @@ const PRESALE_WHITELISTED_ADDRESSES = ["0xb5417fb2931dA5871deF8e063b413C607bd0A7
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
-const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
+const GENERIC = false; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
 const GENERIC_TITLE = CONTRACT_NAME; // Replace with what you want the generic titles to say if you want it to be different from the contract name.
 const GENERIC_DESCRIPTION = "Hows your sdino?"; // Replace with what you want the generic descriptions to say.
 const GENERIC_IMAGE = "https://ipfs.io/ipfs/bafkreibbv5v5wuuygyklba46q4avvqmet4sz35zkxnm5ujcylficltetty"; // Replace with your generic image that will display for all NFTs pre-reveal.
